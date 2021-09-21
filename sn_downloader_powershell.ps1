cebc09ead6a44677dfe474060e7326f39aeb38fd
Patrick McFarland <pmcfarland@users.noreply.github.com>
2021-09-21 13:33:31 -0400

    fixed offset for episode number
    At some point between that last time I tried this (08/2020) and now Steve changed the html for his site and so my rather crude method for parsing to get the latest episode number was breaking. The offset has been increased to compensate.

--- sn_downloader_powershell.ps1
+++ sn_downloader_powershell.ps1
@@ -1,5 +1,5 @@
 # @ Author: pmcfarland
-# @ Date: 08.16.2020
+# @ Date: 09.21.2021
 
 # This script downloads the podcast Security Now as well as text transcript and pdf show notes
 # Security Now is hosted here: https://www.grc.com/securitynow.htm
@@ -19,10 +19,17 @@ $tofile = "C:\SN\securitynow.txt"
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($fromfile, $tofile)
 # Now it searches the file for the most recent episode number
-$x = Select-String -Path $tofile -Pattern "<!-- ################################################################################ -->" -Context 0,4 | % {$_.Context.PostContext}
+$x = Select-String -Path $tofile -Pattern "<!-- ################################################################################ -->" -Context 0,7 | % {$_.Context.PostContext}
 $y= $x -match '\"'
 $z= $y.substring(9,3)
 $latest= $z.split()[-1]
+
+# Since this script looks for an exact match of the comment line in the html then shifts down a few lines to find the first episode 
+# number, if the html is changed then the offset (Context 0,7) will need to be changed as well.
+
+# Here is a simple debug checkpoint to output $x and then $latest to double check what the script is seeing.
+# echo $x
+# echo $latest
 
 Import-Module BitsTransfer;
 
